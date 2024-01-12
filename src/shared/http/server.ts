c01539fc4f05c0381bc47bc8f5046fbb92322d96
Gabriela Zanette <gabezanette.dev@gmail.com>
2024-01-12 14:28:55 -0300

    Inicio do projeto: Configuração basica da aplicação e arquivos iniciais

---
+++ src/shared/http/server.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => {
+        if (error instanceof AppError) {
+            return response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal Server Error',
+        });
+    },
+);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000 🐱‍👤');
+});
