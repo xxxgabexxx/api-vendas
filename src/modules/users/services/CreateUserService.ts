e2f3331b3bb1706cb74be0a0c9ed5b67c5e49dd4
Gabriela Zanette <gabezanette.dev@gmail.com>
2024-01-19 15:59:37 -0300

    Melhoria: Criptografia da senha do usuário

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import User from '../typeorm/entities/user';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
+import { hash } from 'bcryptjs';
 
 interface IRequest {
     name: string;
@@ -18,10 +19,12 @@ class CreateUserService {
             throw new AppError('Email address already used.');
         }
 
+        const hashedPassword = await hash(password, 8);
+
         const user = usersRepository.create({
             name,
             email,
-            password,
+            password: hashedPassword,
         });
 
         await usersRepository.save(user);
