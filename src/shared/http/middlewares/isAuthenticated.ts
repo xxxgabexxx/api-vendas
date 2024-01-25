a770e2a7f8b1015b88176c5898a6d846e9a31474
Gabriela Zanette <gabezanette.dev@gmail.com>
2024-01-25 16:20:07 -0300

    feat: Sobrescrita do Objeto Request do Express - add o tipo 'user'

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,22 +3,34 @@ import AppError from '@shared/errors/AppError';
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
 export default function isAuthenticated(
     request: Request,
     response: Response,
     next: NextFunction,
-):void {
+): void {
     const authHeader = request.headers.authorization;
 
-    if(!authHeader) {
+    if (!authHeader) {
         throw new AppError('JWT Token is missing.');
     }
 
     const [, token] = authHeader.split(' ');
 
     try {
-        const decodeToken = verify(token, authConfig.jwt.secret)
-        
+        const decodedToken = verify(token, authConfig.jwt.secret);
+
+        const { sub } = decodedToken as TokenPayload;
+
+        request.user = { 
+            id: sub,
+        }
+
         return next();
     } catch {
         throw new AppError('Invalid JWT Token');
