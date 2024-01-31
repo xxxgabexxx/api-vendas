420ee4e8b1388769f6a6e34ad9ff3dee5e998467
Gabriela Zanette <gabezanette.dev@gmail.com>
2024-01-31 16:21:23 -0300

    controller: adiciona UserAvatarController

---
+++ src/modules/users/controllers/UserAvatarController.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from 'express';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+    public async update(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+
+        const updateAvatar = new UpdateUserAvatarService();
+
+        const user = await updateAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file?.filename as string,
+        });
+
+        return response.json(user);
+    }
+}
