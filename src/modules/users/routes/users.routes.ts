1fb362f0a7580444c6acb3d106847ddf85be2bd3
Gabriela Zanette <gabezanette.dev@gmail.com>
2024-02-01 10:27:07 -0300

    feat(routes): adiciona rota de /avatar, vê se o usuário é autenticado, faz upload da imagem e atualiza o avatar do usuário.

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,12 +1,20 @@
+import  uploadConfig  from '@config/upload';
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
 import UsersController from '../controllers/UsersController';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import UserAvatarController from '../controllers/UserAvatarController';
+
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig);
+
 
 usersRouter.get('/', isAuthenticated, usersController.index);
+
 usersRouter.post(
     '/',
     celebrate({
@@ -17,6 +25,13 @@ usersRouter.post(
         },
     }),
     usersController.create,
+);
+
+usersRouter.patch(
+    '/avatar',
+    isAuthenticated,
+    upload.single('avatar'),
+    usersAvatarController.update,
 );
 
 export default usersRouter;
