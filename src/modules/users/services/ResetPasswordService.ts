5a9c3a2ed53872dbc6fa47b7271046e1abf66ba5
Gabriela Zanette <gabezanette.dev@gmail.com>
2024-02-08 19:21:05 -0300

    feat(token): adiciona Serviço para Redefinição de Senha- ResetPasswordService.ts em src/modules/users/services

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,27 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+    email: string;
+}
+
+class ResetPasswordService {
+    public async execute({ email }: IRequest): Promise<void> {
+        const usersRepository = getCustomRepository(UsersRepository);
+        const userTokenRepository = getCustomRepository(UserTokensRepository);
+
+        const user = await usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new AppError('User does not exist.');
+        }
+
+        const token = await userTokenRepository.generate(user.id);
+        
+        console.log(token);
+        
+    }
+}
+export default ResetPasswordService;
